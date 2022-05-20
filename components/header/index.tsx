ae5b2635107d5f2ed7ff6920e8d60621ee575515
mojtaba beheshti <moji2002@gmail.com>
2022-05-20 23:22:35 +0430

    logo and social links

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,7 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { FC } from "react";
 import s from "./style.module.scss";
 
-const Header = () => {
-  return <div className={s.container}>hello</div>;
+type Props = {
+  // children: ReactNode;
+};
+
+const Header: FC<Props> = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.leftHand}>
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <img className={s.icon} src="/assets/icons/linkedin.svg" alt="" />
+        </a>{" "}
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <img className={s.icon} src="/assets/icons/github.svg" alt="" />
+        </a>
+      </div>
+      <div className={s.logo}>Philosophy.</div>
+      <div className={s.rightHand}>
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+          <img className={s.icon} src="/assets/icons/search.svg" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
