5bdcd84de8bdc4629a601c83cac66f745251efbe
Mojtaba <moji2002@gmail.com>
2022-06-02 23:27:08 +0430

    feat:random posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,17 @@ import NavMobile from "../components/NavMobile";
 import PageHeader from "../components/PageHeader";
 import Search from "../components/Search";
 import PageLoader from "../components/PageLoader";
+import MasonryGrid from "../components/MasonryGrid";
+import MasonryBrick from "../components/MasonryBrick";
+import getRandomInt from "../utils/getRandomInt";
+import lorem from "../utils/lorem";
+import type { PostCardType } from "../types/PostCardType";
 
-const Home: NextPage = () => {
+type Props = {
+  posts: PostCardType[];
+};
+
+const Home: NextPage<Props> = ({ posts }) => {
   return (
     <>
       <Head>
@@ -17,15 +26,48 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PageHeader>
-        <Header />
         <NavDesktop />
         <NavMobile />
         <Search />
-        <Hero />
+        {/* <Hero /> */}
         <PageLoader />
+        <MasonryGrid>
+          {posts.map(({ imgSrc, excerpt, title, date, index }) => {
+            return (
+              <MasonryBrick
+                key={title}
+                title={title}
+                excerpt={excerpt}
+                date={date}
+                imgSrc={imgSrc}
+                metaLinks={[{ href: "", label: "" }]}
+                index={index}
+              />
+            );
+          })}
+        </MasonryGrid>
       </PageHeader>
     </>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const posts = [...Array(20).keys()].map((index) => {
+    const imgSeed = getRandomInt(1, 1000);
+    const imgHeight = getRandomInt(200, 500);
+    const title = lorem.generateSentences(1);
+    const excerpt = lorem.generateParagraphs(1);
+    return {
+      imgSrc: `https://picsum.photos/seed/${imgSeed}/400/${imgHeight}`,
+      excerpt,
+      title,
+      date: Date.now(),
+      index,
+    };
+  });
+  return {
+    props: { posts },
+  };
+}
